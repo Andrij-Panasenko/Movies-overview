6e7b4b8ad50729dbd9c2bce419fec362ad354257
a.panasenko <andrii.panasenko295@gmail.com>
2023-11-25 14:04:10 +0200

    D

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,5 +1,14 @@
-import { BASE_POSTER_URL, IMG_PLSCEHOLDER_LARGE } from 'constants/constants';
-import { GenreItem, Genres, GenresList, Img, InfoWrapp, MovieTitle, Overview, Wrapper } from './MovieDetails.styled';
+import { BASE_POSTER_URL, POSTER_LARGE_PLACEHOLDER } from 'constants/constants';
+import {
+  GenreItem,
+  Genres,
+  GenresList,
+  Img,
+  InfoWrapp,
+  MovieTitle,
+  Overview,
+  Wrapper,
+} from './MovieDetails.styled';
 
 export const MovieDetails = ({ movie }) => {
   return (
@@ -10,13 +19,13 @@ export const MovieDetails = ({ movie }) => {
             src={
               movie.poster_path
                 ? BASE_POSTER_URL + movie.poster_path
-                : IMG_PLSCEHOLDER_LARGE
+                : POSTER_LARGE_PLACEHOLDER
             }
             alt={movie.original_title}
           />
           <InfoWrapp>
             <MovieTitle>{movie.original_title}</MovieTitle>
-            <p >User score: {movie.vote_average}</p>
+            <p>User score: {movie.vote_average}</p>
             <div>
               <Overview>Overview</Overview>
               <p>{movie.overview}</p>
@@ -25,8 +34,9 @@ export const MovieDetails = ({ movie }) => {
               <Genres>Genres</Genres>
               <GenresList>
                 {movie.genres &&
-                  movie.genres.map(genre =>
-                    <GenreItem key={genre.id}>{genre.name}</GenreItem>)}
+                  movie.genres.map(genre => (
+                    <GenreItem key={genre.id}>{genre.name}</GenreItem>
+                  ))}
               </GenresList>
             </div>
           </InfoWrapp>
