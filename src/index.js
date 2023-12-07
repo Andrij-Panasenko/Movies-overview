27df47ae3b0b00ee61f6bf551c183a72bd95910d
a.panasenko <andrii.panasenko295@gmail.com>
2023-12-07 15:37:40 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const theme = {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="goit-react-hw-05-movies">
+    <BrowserRouter basename="Movies-overview">
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
